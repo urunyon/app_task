ff8a1fda6388a6d30c9359357d7c6b7c8d8f34b2
urunyon <141293154+urunyon@users.noreply.github.com>
2023-09-02 11:08:06 +0000

    [Add]show_favorite

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,7 +1,7 @@
 class Book < ApplicationRecord
   has_one_attached :image
   belongs_to :user
-  has_many :favorites, dependent: destroy
+  has_many :favorites, dependent: :destroy
   validates :title,presence:true
   validates :body,presence:true,length:{maximum:200}
 
