dabafef3954635dc489d120a051e177a98bd595d
urunyon <141293154+urunyon@users.noreply.github.com>
2023-09-02 13:27:11 +0000

    [Finish]応用課題3

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,8 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_one_attached :profile_image
-  has_many :books
+  has_many :books, dependent: :destroy
+  has_many :book_comments, dependent: :destroy
   has_many :favorites, dependent: :destroy
 
   validates :name, length: { minimum: 2, maximum: 20 }, uniqueness: true
