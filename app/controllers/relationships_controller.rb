4a49e363d0dbff95032172738b9186882270e0d2
urunyon <141293154+urunyon@users.noreply.github.com>
2023-09-03 10:41:14 +0000

    [Add]relationships_controller

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,11 +1,26 @@
 class RelationshpsController < ApplicationController
+  before_action :authenticate_user!#アクション実行前にユーザーがログインしとるか確認してる
   
-  def create
-  
+  def create #フォローするアクション
+    user = User.find(params[:user_id])
+    current_user.follow(user)#カレントユーザーが.ユーザーをフォロー
+    redirect_to request.referer#アクション実行前のページを表示（アクション実行してもページが変わらない）
   end
   
-  def destroy
-    
+  def destroy #フォロー解除するアクション
+    user = User.find(params[:user_id])
+    current_user.unfollow(user)#カレントユーザーが.ユーザーのフォローを解除
+    redirect_to request.referer
+  end
+  
+  def followings #特定のユーザー「が」フォローしているユーザーの一覧を表示するアクション
+    user = User.find(params[:user_id])#対象のユーザーをユーザー達から特定
+    @user = user.followings#上で特定したユーザー「が」フォローしているユーザーを@userに代入してる
+  end
+  
+  def followers #特定のユーザー「を」フォローしているユーザーの一覧を表示するアクション
+    user = User.find(params[:user_id])#対象のユーザーをユーザー達から特定
+    @user = user.followers#上で特定したユーザー「を」フォローしているユーザーを@userに代入してる
   end
   
 end
