a0b9c6bb6775f37037c7ee5480a805fe7dcbbeac
urunyon <141293154+urunyon@users.noreply.github.com>
2023-09-05 10:23:39 +0000

    [error]Nameerror

--- app/models/book.rb
+++ app/models/book.rb
@@ -6,6 +6,18 @@ class Book < ApplicationRecord
   validates :title,presence:true
   validates :body,presence:true,length:{maximum:200}
 
+  def self.search_for(content, method)
+    if method == 'perfect'
+      Book.where(title: content)
+    elsif method == 'forward'
+      Book.where('name LIKE ?', content + '%')
+    elsif method == 'backward'
+      Book.where('name LIKE ?', '%' + content)
+    else
+      Book.where('name LIKE ?', '%' + content + '%')
+    end
+  end
+
   def get_image
     unless image.attached?
       file_path = Rails.root.join('app/assets/images/no_image.jpg')
