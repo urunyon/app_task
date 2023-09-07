51b60cd0e607d433ba9a323409fb5b105aa7dc8d
urunyon <141293154+urunyon@users.noreply.github.com>
2023-09-07 10:23:27 +0000

    [Finish]task6

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -2,13 +2,15 @@ class BookCommentsController < ApplicationController
 
   def create
     book = Book.find(params[:book_id])
-    comment = current_user.book_comments.new(book_comment_params)
-    comment.book_id = book.id
-    comment.save
+    @book = Book.find(params[:book_id])
+    @comment = current_user.book_comments.new(book_comment_params)
+    @comment.book_id = book.id
+    @comment.save
   end
 
   def destroy
     comment = BookComment.find(params[:id])
+    @comment = BookComment.find(params[:id])
     comment.destroy
   end
   
--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -2,13 +2,11 @@ class FavoritesController < ApplicationController
   
   def create
     @book = Book.find(params[:book_id])
-    book = Book.find(params[:book_id])
     favorite = @book.favorites.new(user_id: current_user.id)
     favorite.save
   end
   def destroy
     @book = Book.find(params[:book_id])
-    book = Book.find(params[:book_id])
     favorite = current_user.favorites.find_by(book_id: @book.id)
     favorite.destroy
   end
