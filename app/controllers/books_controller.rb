617057099c0f640be5d1a28ca9674bae5095d636
urunyon <141293154+urunyon@users.noreply.github.com>
2023-10-30 01:53:07 +0000

    [Add]favorite_sort

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -15,7 +15,12 @@ class BooksController < ApplicationController
     elsif params[:star_count]
      @books = Book.star_count
     else
-     @books = Book.all
+     to = Time.current.at_end_of_day
+     from = (to - 6.day).at_beginning_of_day
+     @books = Book.includes(:favorited_users).
+       sort_by {|x|
+         x.favorited_users.includes(:favorites).where(created_at: from...to).size
+       }.reverse
     end
     
     @book = Book.new
