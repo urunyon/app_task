ccdb890fe35854d488fae84b908b6d39c0f817c4
urunyon <141293154+urunyon@users.noreply.github.com>
2023-10-31 12:47:47 +0000

    [Add]group

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,6 +15,7 @@ class User < ApplicationRecord
   has_many :entries
   has_many :messages
   has_many :view_counts, dependent: :destroy
+  has_many :group_users, dependent: :destroy
 
   validates :name, length: { minimum: 2, maximum: 20 }, uniqueness: true
   validates :introduction, length: { maximum: 50 }
--- config/routes.rb
+++ config/routes.rb
@@ -19,5 +19,6 @@ Rails.application.routes.draw do
   end
   resources :messages, only: [:create]
   resources :rooms, only: [:create, :show]
+  resources :groups, except: [:destroy]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
